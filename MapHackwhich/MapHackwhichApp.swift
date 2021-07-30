1f51f5a5183038e0f2db85fd0654a625e41dedb2
Bruno Garcia <bthemaker@gmail.com>
2021-07-30 11:40:40 -0400

    Initial Commit

---
+++ MapHackwhich/MapHackwhichApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapHackwhichApp.swift
+//  MapHackwhich
+//
+//  Created by Bruno Garcia on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct MapHackwhichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
